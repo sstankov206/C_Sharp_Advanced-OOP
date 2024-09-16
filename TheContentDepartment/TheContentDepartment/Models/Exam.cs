e4ff40bfa1eb6740d71896754976d8afafcb37e1
sstankov <stankovst@gmail.com>
2024-09-16 18:20:16 +0300

    ADD: Ex: 'The Content Department' update 1

---
+++ TheContentDepartment/TheContentDepartment/Models/Exam.cs
@@ -0,0 +1,13 @@
+﻿namespace TheContentDepartment.Models
+{
+    public class Exam : Resource
+    {
+        private const int priority = 1;
+
+        public Exam(string name, string creator)
+            : base(name, creator, priority)
+        {
+
+        }
+    }
+}
