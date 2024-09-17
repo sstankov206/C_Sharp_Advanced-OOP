505748875ed3bdd4226b909d6f70c4e130b85781
sstankov <stankovst@gmail.com>
2024-09-17 15:44:27 +0300

    ADD: Ex: 'The Content Department' update 2

---
+++ TheContentDepartment/TheContentDepartment/Models/Workshop.cs
@@ -0,0 +1,14 @@
+﻿
+namespace TheContentDepartment.Models
+{
+    public class Workshop : Resource
+    {
+        private const int priority = 2;
+
+        public Workshop(string name, string creator)
+            : base(name, creator, priority)
+        {
+
+        }
+    }
+}
