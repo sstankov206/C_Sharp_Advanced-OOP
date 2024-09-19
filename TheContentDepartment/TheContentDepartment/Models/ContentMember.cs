ecabd1a8e2d8222411612c45876f5f5e4e91a14f
sstankov <stankovst@gmail.com>
2024-09-19 17:56:11 +0300

    ADD: Exercise 'The Content Department' Update 4

---
+++ TheContentDepartment/TheContentDepartment/Models/ContentMember.cs
@@ -0,0 +1,22 @@
+﻿using TheContentDepartment.Utilities.Messages;
+
+namespace TheContentDepartment.Models
+{
+    public class ContentMember : TeamMember
+    {
+        public ContentMember(string name, string path)
+            : base(name, path)
+        {
+            if (path != "Python" && path != "CSharp" && path != "Java" && path != "JavaScript")
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.PathIncorrect, path));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Path} path. Currently working on {InProgress.Count} tasks.";
+        }
+    }
+}
+
---
+++ TheContentDepartment/TheContentDepartment/Repositories/ResourceRepository.cs
@@ -0,0 +1,24 @@
+﻿using TheContentDepartment.Models.Contracts;
+using TheContentDepartment.Repositories.Contracts;
+
+namespace TheContentDepartment.Repositories
+{
+    public class ResourceRepository : IRepository<IResource>
+    {
+        private List<IResource> models;
+
+        public ResourceRepository()
+        {
+            models = new List<IResource>();
+        }
+
+        public IReadOnlyCollection<IResource> Models => models.AsReadOnly();
+
+        public void Add(IResource model)
+        {
+            models.Add(model);
+        }
+
+        public IResource TakeOne(string modelName) => models.FirstOrDefault(m => m.Name == modelName);
+    }
+}
